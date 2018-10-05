63ce9128ff031379478a9a095982732d04cb6d34
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-05 14:26:00 +0200

    Copied to new ui-library

---
+++ jrlgreetings.FormsUI/Pages/ExceptionalPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using jrlgreetings.Core.ViewModels;
+using MvvmCross.Forms.Core;
+using MvvmCross.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jrlgreetings.Core.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ExceptionalPage : MvxContentPage
+	{
+		public ExceptionalPage ()
+		{
+			InitializeComponent ();
+		}
+    }
+}
---
+++ jrlgreetings.FormsUI/Pages/SouthWestPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.Forms.Core;
+using MvvmCross.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jrlgreetings.Core.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SouthWestPage : MvxContentPage
+	{
+		public SouthWestPage ()
+		{
+			InitializeComponent ();
+            certImage.Source = ImageSource.FromResource("jrlgreetings.Core.xamarin_mobile_developer.png", typeof(SouthWestPage).GetTypeInfo().Assembly);
+        }
+
+        private void certImage_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://devconnect.xamarin.com/profile/861"));
+        }
+    }
+}
