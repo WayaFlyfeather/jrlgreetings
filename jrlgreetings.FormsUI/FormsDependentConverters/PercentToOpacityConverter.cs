4b6acceb29bf33135e42b0e01ae6b0f1cf979cba
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-07 01:06:00 +0200

    Fixing problems with Opacity and Rotation values in NorthWest and North rooms not being bound correctly intermittently and randomly - problem seems to be in UWP when bound through MvvmCross; changed to standard Forms binding, and changed or discarded converters; at the same time changing special Android rotation value to same as iOS and UWP.

---
+++ jrlgreetings.FormsUI/FormsDependentConverters/PercentToOpacityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace jrlgreetings.FormsUI.FormsDependentConverters
+{
+    public class PercentToOpacityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; } = false;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dValue = System.Convert.ToDouble(value);
+            return Reversed ? (100.0 - dValue) / 100.0 : dValue / 100.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
