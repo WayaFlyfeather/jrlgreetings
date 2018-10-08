f867e228ab4ed1004fa1eb9950aa018db5a2f16f
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-09 00:29:58 +0200

    Building starting frame

---
+++ jrlgreetings.WPF/ServiceImpl/WPFSoundPlayerService.cs
@@ -0,0 +1,27 @@
+﻿using jrlgreetings.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jrlgreetings.WPF.ServiceImpl
+{
+    public class WPFSoundPlayerService : ISoundPlayerService
+    {
+        public void PlayClick()
+        {
+            
+        }
+
+        public void PlayFootsteps()
+        {
+            
+        }
+
+        public void PlayThunder()
+        {
+            
+        }
+    }
+}
---
+++ jrlgreetings.WPF/Setup.cs
@@ -0,0 +1,21 @@
+﻿using jrlgreetings.Core.Services;
+using jrlgreetings.WPF.ServiceImpl;
+using MvvmCross;
+using MvvmCross.Platforms.Wpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jrlgreetings.WPF
+{
+    public class Setup : MvxWpfSetup<Core.CoreApp>
+    {
+        protected override void InitializePlatformServices()
+        {
+            base.InitializePlatformServices();
+            Mvx.IoCProvider.RegisterSingleton<ISoundPlayerService>(new WPFSoundPlayerService());
+        }
+    }
+}
