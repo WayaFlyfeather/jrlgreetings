b05ffe0a39c36f508f477af267fb8c6c28e29377
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 01:30:09 +0200

    Starting implementing EastView

--- jrlgreetings.WPF/Views/EastView.xaml.cs
+++ jrlgreetings.WPF/Views/EastView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace jrlgreetings.WPF.Views
     /// <summary>
     /// Interaction logic for EastView.xaml
     /// </summary>
-    public partial class EastView : UserControl
+    public partial class EastView : MvxWpfView
     {
         public EastView()
         {
