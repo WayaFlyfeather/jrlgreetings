caab382309d1d4cb2ede33188666a143a1b36ba0
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-09 16:25:16 +0200

    Adding PercentToOpacityConverter

---
+++ jrlgreetings.WPF/WPFConverters/PercentToOpacityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class PercentToOpacityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dValue = System.Convert.ToDouble(value);
+            return Reversed ? (100.0 - dValue) / 100.0 : dValue / 100.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
