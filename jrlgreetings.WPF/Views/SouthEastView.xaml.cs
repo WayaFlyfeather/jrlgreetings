fd36dff2ecce05b529250346d6ac1df533e8f495
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 01:35:27 +0200

    Implementing SouthEastView.

--- jrlgreetings.WPF/Views/SouthEastView.xaml.cs
+++ jrlgreetings.WPF/Views/SouthEastView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace jrlgreetings.WPF.Views
     /// <summary>
     /// Interaction logic for SouthEastView.xaml
     /// </summary>
-    public partial class SouthEastView : UserControl
+    public partial class SouthEastView : MvxWpfView
     {
         public SouthEastView()
         {
