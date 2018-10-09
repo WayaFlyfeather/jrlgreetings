9ce253bc8be4a3688cace0c20b2a92226c48a292
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-09 19:56:39 +0200

    Adding StringToBrokenString converter

---
+++ jrlgreetings.WPF/WPFConverters/StringToBrokenStringConverter.cs
@@ -0,0 +1,14 @@
+﻿using jrlgreetings.Core.Pages;
+using MvvmCross.Platforms.Wpf.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class StringToBrokenStringConverter : MvxNativeValueConverter<MvxStringToBrokenStringConverter>
+    {
+    }
+}
