43511cafe921c3db7cad7d99f7ee19764eeabcbf
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-09 18:30:46 +0200

    Adding ZeroToVisibilityConverter.

---
+++ jrlgreetings.WPF/WPFConverters/ZeroToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class ZeroToVisibilityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dValue = System.Convert.ToDouble(value);
+            if (Reversed)
+                return dValue == 0.0 ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return dValue != 0.0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
