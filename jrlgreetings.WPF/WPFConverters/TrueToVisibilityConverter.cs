005b922c13bbd24f31a874e6b11544fc01260271
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 16:06:12 +0200

    Adding TrueToVisibilityConverter.

---
+++ jrlgreetings.WPF/WPFConverters/TrueToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class TrueToVisibilityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Reversed)
+                return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
