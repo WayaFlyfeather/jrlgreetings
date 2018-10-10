c075cd88c43108903b82adc34d150b2b8d46bbb4
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 11:27:50 +0200

    Implementing SouthView

--- jrlgreetings.WPF/Views/SouthView.xaml.cs
+++ jrlgreetings.WPF/Views/SouthView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace jrlgreetings.WPF.Views
     /// <summary>
     /// Interaction logic for SouthView.xaml
     /// </summary>
-    public partial class SouthView : UserControl
+    public partial class SouthView : MvxWpfView
     {
         public SouthView()
         {
