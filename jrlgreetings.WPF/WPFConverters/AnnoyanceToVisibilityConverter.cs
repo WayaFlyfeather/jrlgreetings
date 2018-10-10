926f2a32440eff5898f65e91e6ffe51ad6572b47
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 11:19:55 +0200

    Adding AnnoyanceToVisibilityConverter

---
+++ jrlgreetings.WPF/WPFConverters/AnnoyanceToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class AnnoyanceToVisibilityConverter : IValueConverter
+    {
+        double[] threshold = new double[] { 5.0, 19.0, 33.0, 47.0, 61.0, 75.0, 89.0 };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dValue = System.Convert.ToDouble(value);
+            byte veilNo = 1;
+            try
+            {
+                veilNo = System.Convert.ToByte(parameter);
+            }
+            catch (Exception) { }
+
+            if (dValue < threshold[7 - veilNo])
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
