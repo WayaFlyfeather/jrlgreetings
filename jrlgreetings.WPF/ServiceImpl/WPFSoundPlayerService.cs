561bdb16d71f8db69e0495b8fab25f6eb0a80008
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-10 15:46:18 +0200

    Adding sounds.

--- jrlgreetings.WPF/ServiceImpl/WPFSoundPlayerService.cs
+++ jrlgreetings.WPF/ServiceImpl/WPFSoundPlayerService.cs
@@ -4,24 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace jrlgreetings.WPF.ServiceImpl
 {
     public class WPFSoundPlayerService : ISoundPlayerService
     {
+        MediaPlayer clickPlayer;
+        MediaPlayer footstepsPlayer;
+        MediaPlayer thunderPlayer;
+
+        public WPFSoundPlayerService()
+        {
+            clickPlayer = new MediaPlayer();
+            clickPlayer.Open(new Uri("Resources/click.mp3", UriKind.Relative));
+
+            footstepsPlayer = new MediaPlayer();
+            footstepsPlayer.Open(new Uri("Resources/footsteps.mp3", UriKind.Relative));
+
+            thunderPlayer = new MediaPlayer();
+            thunderPlayer.Open(new Uri("Resources/thunder.mp3", UriKind.Relative));
+        }
+
         public void PlayClick()
         {
-            
+            clickPlayer.Stop();
+            clickPlayer.Play();
         }
 
         public void PlayFootsteps()
         {
-            
+            footstepsPlayer.Stop();
+            footstepsPlayer.Play();
         }
 
         public void PlayThunder()
         {
-            
+            thunderPlayer.Stop();
+            thunderPlayer.Play();
         }
     }
 }
