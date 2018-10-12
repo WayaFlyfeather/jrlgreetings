5b339be8e8b888078aeb3dd8db5ac8a273ba466b
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-12 18:40:40 +0200

    Changing background converter algorithm for the SouthWest challenge

--- jrlgreetings.WPF/WPFConverters/AnnoyanceToBackgroundBrushConverter.cs
+++ jrlgreetings.WPF/WPFConverters/AnnoyanceToBackgroundBrushConverter.cs
@@ -13,10 +13,17 @@ namespace jrlgreetings.WPF.WPFConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            //bool pTempleCompleted = System.Convert.ToBoolean(parameter);
+
             double dValue = System.Convert.ToDouble(value);
+
+            //if (pTempleCompleted && dValue < 0.15)
+            //    return App.Current.Resources["TempleCompletedContentTextBackgroundColor"];
+
             double annoyanceRemovedSq = Math.Pow(100.0 - dValue, 2.0);
             float colorParts = (float)(.5 + (.00005 * annoyanceRemovedSq));
-            Color color = Color.FromScRgb(1.0f, colorParts, colorParts, colorParts);
+            float transparencyPart = (float)(1.0 - (.0001 * annoyanceRemovedSq));
+            Color color = Color.FromScRgb(Math.Max(transparencyPart, .25f), colorParts, colorParts, colorParts);
 
             return new SolidColorBrush(color);
         }
