3c8ced4f3b3a00a2ffd8b582c8f95de8fbf17d7b
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-12 11:27:51 +0200

    Adding CompletionToButtonStyleConverter

---
+++ jrlgreetings.WPF/WPFConverters/CompletionToButtonStyleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class CompletionToButtonStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (System.Convert.ToBoolean(value))
+                return App.Current.Resources["CompletedRoomTeleportButtonStyle"];
+            else
+                return App.Current.Resources["TeleportButtonStyle"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
