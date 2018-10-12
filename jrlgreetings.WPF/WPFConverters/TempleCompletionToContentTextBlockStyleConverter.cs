fe540d115929953fc33435b5a4eaf7aa36455e86
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-12 16:22:02 +0200

    Adding TempleCompletionToContentTextBlockStyleConverter.

---
+++ jrlgreetings.WPF/WPFConverters/TempleCompletionToContentTextBlockStyleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace jrlgreetings.WPF.WPFConverters
+{
+    public class TempleCompletionToContentTextBlockStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (System.Convert.ToBoolean(value))
+                return App.Current.Resources["TempleCompletedRoomContentTextBlockStyle"];
+            else
+                return App.Current.Resources["RoomContentTextBlockStyle"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
