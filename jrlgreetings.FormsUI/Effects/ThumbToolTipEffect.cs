6968087e9aa9cdd8f690496cc2038df8fd18cedb
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-18 09:28:23 +0200

    Adding ThumbToolTipEffect to suppress the ThumbToolTip on Sliders in UWP. Not relevant in Android or iOS.

---
+++ jrlgreetings.FormsUI/Effects/ThumbToolTipEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace jrlgreetings.FormsUI.Effects
+{
+    /// <summary>
+    /// For suppressing a default ThumToolTip on a Slider. Only relevant for UWP.
+    /// </summary>
+    public class ThumbToolTipEffect : RoutingEffect
+    {
+        public bool Suppressed { get; set; } = true;
+
+        public ThumbToolTipEffect() : base("JonRLevy.ThumbToolTipEffect")
+        { }
+    }
+}
---
+++ jrlgreetings.UWP/Effects/ThumbToolTipEffectImpl.cs
@@ -0,0 +1,39 @@
+﻿using jrlgreetings.FormsUI.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: Xamarin.Forms.ExportEffect(typeof(jrlgreetings.UWP.Effects.ThumbToolTipEffectImpl), "ThumbToolTipEffect")]
+namespace jrlgreetings.UWP.Effects
+{
+    public class ThumbToolTipEffectImpl : PlatformEffect
+    {
+        bool previousIsThumbToolTipEnabled = true;
+
+        protected override void OnAttached()
+        {
+            ThumbToolTipEffect thumbToolTipEffect = (ThumbToolTipEffect)Element.Effects.First(e => e is ThumbToolTipEffect);
+
+            switch (Control)
+            {
+                case FormsSlider formsSlider:
+                    previousIsThumbToolTipEnabled = formsSlider.IsThumbToolTipEnabled;
+                    formsSlider.IsThumbToolTipEnabled = !thumbToolTipEffect.Suppressed;
+                    break;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            switch (Control)
+            {
+                case FormsSlider formsSlider:
+                    formsSlider.IsThumbToolTipEnabled = previousIsThumbToolTipEnabled;
+                    break;
+            }
+        }
+    }
+}
