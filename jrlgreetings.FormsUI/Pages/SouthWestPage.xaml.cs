112898d6a0455f62bee6f6b29dab3e08ca987292
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-11-23 14:46:28 +0100

    Updating SoutWest certification Image to new ImageButton

--- jrlgreetings.FormsUI/Pages/SouthWestPage.xaml.cs
+++ jrlgreetings.FormsUI/Pages/SouthWestPage.xaml.cs
@@ -18,10 +18,10 @@ namespace jrlgreetings.FormsUI.Pages
 		public SouthWestPage ()
 		{
 			InitializeComponent ();
-            certImage.Source = ImageSource.FromResource("jrlgreetings.FormsUI.xamarin_mobile_developer.png", typeof(SouthWestPage).GetTypeInfo().Assembly);
+            certImageButton.Source = ImageSource.FromResource("jrlgreetings.FormsUI.xamarin_mobile_developer.png", GetType().Assembly);
         }
 
-        private void certImage_Tapped(object sender, EventArgs e)
+        private void certImageButton_Clicked(object sender, EventArgs e)
         {
             Device.OpenUri(new Uri("https://devconnect.xamarin.com/profile/861"));
         }
