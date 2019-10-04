c8c4106a457738244a799600234fd8b33fdb5e3a
Jon Robinson Levy <jon.r.levy@gmail.com>
2019-10-04 09:04:43 +0200

    Creating Native Droid project

---
+++ jrlgreetings.Native.Droid/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using jrlgreetings.Core;
+using MvvmCross.Platforms.Android.Core;
+using MvvmCross.Platforms.Android.Views;
+
+namespace jrlgreetings.Native.Droid
+{
+    [Application]
+    public class MainApplication : MvxAndroidApplication<MvxAndroidSetup<CoreApp>, CoreApp>
+    {
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+    }
+}
