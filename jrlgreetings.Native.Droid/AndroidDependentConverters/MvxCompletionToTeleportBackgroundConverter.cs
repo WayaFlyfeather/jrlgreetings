b34f73f677e92cfc67707201b2ad2b848960c2e3
Jon Robinson Levy <jon.r.levy@gmail.com>
2019-10-10 19:07:50 +0200

    Adding converter for TeleportButton background, setting color depending on destination room completion

---
+++ jrlgreetings.Native.Droid/AndroidDependentConverters/MvxCompletionToTeleportBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Converters;
+
+namespace jrlgreetings.Native.Droid.AndroidDependentConverters
+{
+    public class MvxCompletionToTeleportBackgroundConverter : MvxValueConverter<bool, ColorDrawable>
+    {
+        protected override ColorDrawable Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value
+                ? new ColorDrawable(new Color(0x7f, 0xff, 0xd4, 0x40))
+                : new ColorDrawable(new Color(0x30, 0x30, 0x30, 0x40));
+        }
+    }
+}
