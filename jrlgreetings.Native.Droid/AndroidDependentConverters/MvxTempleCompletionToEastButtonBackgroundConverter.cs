d4fccf5d6eca52f57023749a3eba26bb69336154
Jon Robinson Levy <jon.r.levy@gmail.com>
2019-10-11 13:33:52 +0200

    Adding TempleCompleted gradient direction button backgrounds + converters

---
+++ jrlgreetings.Native.Droid/AndroidDependentConverters/MvxTempleCompletionToEastButtonBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Converters;
+
+namespace jrlgreetings.Native.Droid.AndroidDependentConverters
+{
+    public class MvxTempleCompletionToEastButtonBackgroundConverter : MvxValueConverter<bool, Drawable>
+    {
+        protected override Drawable Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value
+                ? Application.Context.Resources.GetDrawable(Resource.Drawable.tb_blue_green_gradient, null)
+                : new ColorDrawable(new Color(0x30, 0x30, 0x30, 0x40));
+        }
+    }
+}
---
+++ jrlgreetings.Native.Droid/AndroidDependentConverters/MvxTempleCompletionToNorthButtonBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Converters;
+
+namespace jrlgreetings.Native.Droid.AndroidDependentConverters
+{
+    public class MvxTempleCompletionToNorthButtonBackgroundConverter : MvxValueConverter<bool, Drawable>
+    {
+        protected override Drawable Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value
+                ? Application.Context.Resources.GetDrawable(Resource.Drawable.lr_red_blue_gradient, null)
+                : new ColorDrawable(new Color(0x30, 0x30, 0x30, 0x40));
+        }
+    }
+}
