ed0ec9b432d4f7cf6bd44b0534ab961d5446225f
Jon Robinson Levy <jon.r.levy@gmail.com>
2019-10-14 04:27:15 +0200

    Adding EntranceView to WPF

---
+++ jrlgreetings.WPF/Views/EntranceView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jrlgreetings.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for EntranceView.xaml
+    /// </summary>
+    public partial class EntranceView : MvxWpfView
+    {
+        public EntranceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
